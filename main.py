5279e4189e63498b3c4a909327b7d2611e54796c
rajrayala <rajrayala@gmail.com>
2024-06-20 16:33:53 -0400

    move compare json to new file

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
 import os
-from deepdiff import DeepDiff
-from deepdiff.model import PrettyOrderedSet
 from fetcher import fetch_clean_content
+from compare_json import compare_json
 from file_utils import read_urls_from_csv, save_json, load_json, sanitize_filename, update_change_log
 from github_utils import update_repo
-
-def compare_json(new_data, old_data):
-    diff = DeepDiff(old_data, new_data, ignore_order=True).to_dict()
-    return convert_diff_to_serializable(diff)
-
-def convert_diff_to_serializable(diff):
-    if isinstance(diff, dict):
-        return {k: convert_diff_to_serializable(v) for k, v in diff.items()}
-    elif isinstance(diff, list):
-        return [convert_diff_to_serializable(i) for i in diff]
-    elif isinstance(diff, (set, frozenset)):
-        return list(diff)
-    elif isinstance(diff, PrettyOrderedSet):
-        return list(diff)
-    else:
-        return diff
 
 def main():
     repo_path = os.getenv('GITHUB_WORKSPACE', '.')
